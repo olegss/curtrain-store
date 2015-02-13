cca5d74e15acb8bfc505a46131e19cd927e70b37
Olegss <oll_tv.ukr.net>
2015-02-13 11:25:17 +0200

    edit gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'list/home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'list#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
